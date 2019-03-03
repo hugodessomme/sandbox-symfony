7b75511ab5979c7b092b6c539bc374aff0124420
Hugo <hdessomme49@gmail.com>
2019-03-03 17:04:30 +0100

    Create Markdown service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,12 +2,11 @@
 
 namespace App\Controller;
 
-use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Service\MarkdownHelper;
 
 class ArticleController extends AbstractController
 {
@@ -22,7 +21,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -49,16 +48,7 @@ cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim ca
 fugiat.
 EOF;
 
-        $item = $cache->getItem('markdown_'.md5($articleContent));
-
-        if (!$item->isHit()) {
-            $item->set($markdown->transform($articleContent));
-            $cache->save($item);
-        }
-
-        $articleContent = $item->get();
-
-        dump($markdown);die;
+        $articleContent = $markdownHelper->parse($articleContent);
  
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
